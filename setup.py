ebb823483570a9755717f16a882d02565b6ede77
Sergii Bilyi <sbilyi@lohika.com>
2018-05-15 13:01:36 +0300

    added setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='wazzup',
+      version='1.0',
+      description='Wazzup',
+      author='Sergii Bilyi',
+      author_email='kartanisoid@gmail.com',
+     )
